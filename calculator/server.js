dd62bded332bac98c4d11fdda36a00b2ebf4ba37
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-03 13:08:15 +0200

    Created function for checking token

---
+++ calculator/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const SECRET = "SECRET";
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+
+    req.user = user;
+    next();
+  });
+}
