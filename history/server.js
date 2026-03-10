1ec43345b02c2ac6e1a6cfba6ccff03b32d7e64d
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-10 13:25:20 +0200

    Added dotenv

--- history/server.js
+++ history/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 const History = mongoose.model("History", {
   userId: String,
