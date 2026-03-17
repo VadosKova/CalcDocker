94a0cf2ce442f8d879ae887c37e72fc07b0e1792
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-17 12:35:37 +0200

    Installed redis and added to history

--- history/server.js
+++ history/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { createClient } = require("redis");
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +12,12 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
+
+const redisClient = createClient({
+  url: "redis://redis:6379"
+});
+
+redisClient.connect().catch(console.error);
 
 const History = mongoose.model("History", {
   userId: String,
@@ -24,14 +31,31 @@ const History = mongoose.model("History", {
 app.post("/save", async (req, res) => {
   const operation = new History(req.body);
   await operation.save();
+
+  await redisClient.del(`history:${req.body.userId}`);
   res.json({ message: "Saved" });
 });
 
 app.get("/history/:userId", async (req, res) => {
+  const userId = req.params.userId;
+
+  const cached = await redisClient.get(`history:${userId}`);
+
+  if (cached) {
+    console.log("From Redis");
+    return res.json(JSON.parse(cached));
+  }
+
   const data = await History
     .find({ userId: req.params.userId })
     .sort({ date: -1 });
-  res.json(data);
+  
+  await redisClient.set(
+    `history:${userId}`,
+    JSON.stringify(data),
+    { EX: 60 }
+  );
+  console.log("From MongoDB");
 });
 
 app.listen(6000, () => console.log("History service running"));
